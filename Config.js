ca17db23c229a91ebe85602dc1b03dd89010b8ed
surdarkweb <ben639405@gmail.com>
2025-01-12 11:43:10 +0100

    Update Config.js

--- Config.js
+++ Config.js
@@ -1,19 +1,19 @@
 // hey there, kindly read this file till the end for better configuration
 
 global.settings = {
-  OWNER_NUMBERS: "2349067339193", // your number with country code
-  OWNER_NAME: 'Miracle', // your owner name 
-  BOT_NAME: "KORD", // your preferred bot name
-  SESSION_ID: 'your_sessionid', // replace your_sessionid with yours and make sure its inside rhe quotes
+  OWNER_NUMBERS: "2348101699340", // your number with country code
+  OWNER_NAME: 'JERRY', // your owner name 
+  BOT_NAME: "DARK", // your preferred bot name
+  SESSION_ID: 'kord_ai-jOO6rHLndkIR', // replace your_sessionid with yours and make sure its inside rhe quotes
   INVINCIBLE_MODE: false, // toogle invincible mode 
-  WORK_MODE: "Public",
+  WORK_MODE: "private",
   TIME_ZONE: "Africa/Lagos",
   ANTI_VIEWONCE: true,
   READ_ALL_MESSAGES: false,
   AUTO_READ_STATUS: true,
   PREFIX: [".", "/", "!"],
   GROUP_EVENTS: true,
-  WELCOME_MESSAGE: "Hello! I'm KORD-AI, your friendly WhatsApp bot.",
+  WELCOME_MESSAGE: "Hello!I AM DARK KNEEL BEFORE ME",
   DEFAULT_TRANSLATION_LANG: "en",
   COMMAND_COOLDOWN_TIME_IN_MS: 0.01,
   MAX_COMMANDS_PER_MINUTE: 100,
@@ -21,9 +21,9 @@ global.settings = {
   AUTO_LIKE_STATUS: true,
   AUTO_LIKE_EMOJI: '🍁',
   BANKING_DETAILS: {
-    bank: "Kord-Ai Bank", // the name of your preferred bank
-    acc_number: "1234567890", // account number for your preferred bank
-    acc_name: "MyAccountName" // name of your bank
+    bank: "OPAY", // the name of your preferred bank
+    acc_number: "9051034067", // account number for your preferred bank
+    acc_name: "AYIBETU YARO" // name of your bank
   },
   /* You can also replace the numbers with your addresses if you're using crypto, a command for this will come soon. */
   API_KEYS: {
@@ -31,7 +31,7 @@ global.settings = {
   },
   isHackEnable: true,
   SAFE_SEARCH: true,
-  REJECT_CALLS: true,
+  REJECT_CALLS: false,
   SEND_WELCOME_MESSAGE: false,
   WELCOME_MESSAGE: "👋 Welcome to the group! We're glad to have you here. 😊",
   DELETE_WA_LINKS: false,
